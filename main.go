943bc596feace00f0beb2ad6edcda0871b732f21
Timothy Jones <timothy.l.jones@gmail.com>
2015-12-29 17:13:02 +1300

    Now prints out title, docid and url on standard error

--- main.go
+++ main.go
@@ -12,10 +12,18 @@ import (
 const (
 	HEADER = iota
 	HTTPHEADER
+	DOCHEADER
 	HTMLCONTENT
+	HEADER_DOCID
 )
 
 func main() {
+	sOut, err := os.Open("sOut")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer sOut.Close()
+
 	file, err := os.Open("files.trec")
 	if err != nil {
 		log.Fatal(err)
@@ -25,12 +33,24 @@ func main() {
 	scanner := bufio.NewScanner(file)
 	current := HEADER
 
-	htmlcontent := ""
+	var (
+		htmlcontent, docno, doctitle, docurl string
+	)
+
 	for scanner.Scan() {
 		str := scanner.Text()
 		switch current {
+		case HEADER_DOCID:
+			fmt.Println(str)
+			docno = str[7:][:25]
+			current = HEADER
 		case HEADER:
 			fmt.Println(str)
+		case DOCHEADER:
+			fmt.Println(str)
+			if str != "</DOCHDR>" {
+				docurl = str
+			}
 		case HTTPHEADER:
 		case HTMLCONTENT:
 			if str != "</DOC>" {
@@ -40,7 +60,9 @@ func main() {
 
 		switch str {
 		case "<DOC>":
-			current = HEADER
+			current = HEADER_DOCID
+		case "<DOCHDR>":
+			current = DOCHEADER
 		case "</DOCHDR>":
 			fmt.Println("<TEXT>")
 			current = HTTPHEADER
@@ -61,6 +83,12 @@ func main() {
 				if n.Type == html.ElementNode && (n.Data == "script" || n.Data == "style" || n.Data == "noscript") {
 					return
 				}
+				if n.Type == html.ElementNode && n.Data == "title" {
+					if n.FirstChild != nil {
+						doctitle = n.FirstChild.Data
+					}
+					return
+				}
 				if n.Type == html.TextNode {
 					fmt.Println(" " + n.Data)
 				}
@@ -69,6 +97,16 @@ func main() {
 				}
 			}
 			f(doc)
+			fmt.Fprintln(os.Stderr, docno)
+			if doctitle == "" {
+				fmt.Fprintln(os.Stderr, "(no title)")
+			} else {
+				fmt.Fprintln(os.Stderr, doctitle)
+			}
+			fmt.Fprintln(os.Stderr, docurl)
+			docno = ""
+			docurl = ""
+			doctitle = ""
 			fmt.Println("</TEXT>")
 			fmt.Println("</DOC>")
 			current = HEADER
